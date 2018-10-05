3a6e4c1a9bf65fe13a1934e6943a57414d8c0cf0
Gadzhi Kharkharov <me@kkga.me>
2018-10-05 18:41:14 +0300

    update typography

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,5 +1,15 @@
-import Typography from "typography"
+import Typography from 'typography'
 
-const typography = new Typography({ baseFontSize: "18px" })
+const typography = new Typography({
+  googleFonts: [
+    {
+      name: 'Roboto Condensed',
+      styles: ['400', '400i', '700', '700i'],
+    },
+  ],
+  baseFontSize: '18px',
+  headerFontFamily: ['Roboto Condensed', 'sans-serif'],
+  bodyFontFamily: ['Roboto Condensed', 'sans-serif'],
+})
 
 export default typography
